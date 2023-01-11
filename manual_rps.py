ff81b755f6c71ba3f6158b8755b2a0e57e42021e
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-01-11 18:36:55 +0000

    If-elif-else loop added

--- manual_rps.py
+++ manual_rps.py
@@ -9,4 +9,12 @@ def get_computer_choice():
 def get_user_choice():
     user_choice = input('Choose between rock,paper and scissors: ')
     return user_choice
+
+def get_winner(computer_choice,user_choice):
+    if computer_choice == user_choice:
+        return "It is a tie"
+    elif computer_choice =='Rock' and user_choice=='Scissors' or computer_choice=='Paper' and user_choice=='Rock' or computer_choice=='Scissors' and user_choice=='Paper':
+        return 'You lost!'
+    else:
+        return 'You won!'
     
