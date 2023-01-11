18729de351c32829405609617628a4f8a679a38b
ProjectsWithZoe <118231395+ProjectsWithZoe@users.noreply.github.com>
2023-01-11 18:08:26 +0000

    Get computer choice and get user choice functions created

---
+++ manual_rps.py
@@ -0,0 +1,11 @@
+import random
+
+choices =['Rock', 'Paper', 'Scissors']
+def get_computer_choice():
+    computer_choice = random.choice(choices)
+    return computer_choice
+
+def get_user_choice():
+    user_choice = input('Choose between rock,paper and scissors: ')
+    return user_choice
+    
